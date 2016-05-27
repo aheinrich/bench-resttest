d59f70142eb4d79fa4e35c60dbb8e79385df5f76
Aaron Heinrich <nerdx7@gmail.com>
2016-05-27 15:50:20 -0700

    - ugly as heck, but it works for now

--- src/app/transactions/sorting.pipe.ts
+++ src/app/transactions/sorting.pipe.ts
@@ -5,8 +5,43 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class SortingPipe implements PipeTransform {
-    transform(value: any, args: any[]): any {
-        console.log(args)
-        return value;
+    transform(value: any, args: string): any {
+        
+        let sortModeAsc = false;
+        
+        if (!value){
+            return value
+        }
+        
+        if (!args){
+            return value
+        }
+        
+        let sortAsc = (a, b) => {
+            if (a[args] < b[args]) return -1;
+            if (a[args] > b[args]) return 1;
+            return 0
+        }
+        
+        let sortDesc = (a, b) => {
+            if (a[args] < b[args]) return 1;
+            if (a[args] > b[args]) return -1;
+            return 0
+        }
+        
+        let compareFn:any;
+        
+        if (args.startsWith("+")){
+            compareFn = sortAsc
+            args = args.slice(1)
+        } else if (args.startsWith("-")){
+            compareFn = sortDesc
+            args = args.slice(1)
+        } else {
+            compareFn = sortAsc
+        }
+        
+        return value.sort(compareFn)
+
     }
 }
