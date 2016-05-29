f0691bdb09b41a8601dd1f5d7d59e7b10d5a3a91
Aaron Heinrich <nerdx7@gmail.com>
2016-05-29 15:42:01 -0700

    - renamed currency pipe, to reduce misuse from normal 'currency' pipe

--- src/app/transactions/pipes/currency.pipe.ts
+++ src/app/transactions/pipes/currency.pipe.ts
@@ -1,11 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'currency'
+    name: 'basicCurrency'
 })
 
 export class CurrencyPipe implements PipeTransform {
-    transform(value: any, args: any[]): any {
+    transform(value: any, args: string | any[]): any {
+        if (args == "dollars"){
+            value = value/100
+        } 
         return "$" + value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
     }
 
