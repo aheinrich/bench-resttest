bdbe439fdfb897fef2ad2d764e34b42291a01433
Aaron Heinrich <nerdx7@gmail.com>
2016-05-28 22:16:24 -0700

    - moving shared functionality into common Utils class

---
+++ src/app/transactions/models/utilities.ts
@@ -0,0 +1,28 @@
+import {Transaction} from './transactions.models'
+import * as _ from 'lodash'
+import * as moment from 'moment'
+
+export interface ITransactionGroup {
+    date: Date,
+    total: number,
+    transactions: Transaction[]
+}
+
+export class Utilities {
+    
+    static calculateDailyTotals(transactionList: Transaction[]): ITransactionGroup[] {
+        let groupByTimestamp: _.Dictionary<any> = _.groupBy(transactionList, 'dateTimestamp')
+        
+        return Object.keys(groupByTimestamp).map(dateTimestamp => {
+            let dailyTotal = _.reduce(groupByTimestamp[dateTimestamp].map( (t:Transaction) => t.amount), (sum:number ,n:number) => {
+                return sum+n    
+            }, 0)
+            return {
+                date: new Date(Number.parseInt(dateTimestamp)),
+                total: dailyTotal,
+                transactions: groupByTimestamp[dateTimestamp]
+            }
+        })
+    }
+    
+}
