61cad1f5f30c337fffb5b3651a4907f312632eb4
Aaron Heinrich <nerdx7@gmail.com>
2016-06-01 17:19:32 -0700

    - removing compiled source from 'src' folder

--- src/client/app/transactions/pipes/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var currency_pipe_1 = require("./currency.pipe");
-exports.CurrencyPipe = currency_pipe_1.CurrencyPipe;
-var sorting_pipe_1 = require("./sorting.pipe");
-exports.SortingPipe = sorting_pipe_1.SortingPipe;
--- src/client/app/transactions/services/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var transactions_service_1 = require("./transactions.service");
-exports.TransactionService = transactions_service_1.TransactionService;
