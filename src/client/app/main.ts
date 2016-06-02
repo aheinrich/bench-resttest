27580a131f79949b457364aa7636f8fd1e21b22b
Aaron Heinrich <nerdx7@gmail.com>
2016-06-01 17:20:24 -0700

    - Angular2 support for loading config at runtime

--- src/client/app/main.ts
+++ src/client/app/main.ts
@@ -1,14 +1,46 @@
-// import { enableProdMode } from "@angular/core";
+import { enableProdMode } from "@angular/core";
 import { bootstrap } from "@angular/platform-browser-dynamic";
 import { HTTP_PROVIDERS } from "@angular/http";
-import { AppComponent } from "./app.component";
+import { AppComponent, AppSettings, ISettings } from "./index";
 
-// enableProdMode();
 
-fetch(){
-    
+const runBoostrap = (settings:ISettings) => {
+    bootstrap(AppComponent, [
+        HTTP_PROVIDERS,
+    ]).then(
+        success => {
+            if (settings.environment === 'development') console.log(`Bootstrap success`)
+        })
+        .catch(
+        error => {
+            console.log(error)
+        });
 }
-bootstrap(AppComponent, [
-    HTTP_PROVIDERS,
-]).then(success => console.log(`Bootstrap success`))
-.catch(error => console.log(error));
+
+try {
+    // Use Fetch Api to pull down server-provided settings
+    fetch('/settings', { method: 'get' })
+        .then((response) => {
+            return response.json()
+        })
+        .then((settings: ISettings) => {
+
+            // Apply the settings to the Application
+            AppSettings.settings = settings;
+
+            // If platform is set to 'Production' mode
+            if (AppSettings.settings.environment == 'production') {
+                enableProdMode();
+            };
+
+            // Startup the application
+            runBoostrap(settings)
+
+        });
+
+} catch (err) {
+    console.log("Unable to fetch settings from platform")
+    throw err;
+}
+
+
